63640d727ca33c5bddbd83b2332bebf6c70ccd0c
juan <jbiancuzzo@fi.uba.ar>
2020-12-26 21:08:48 -0300

    Agregando lo basico del main.c
    Agrego muchas definiciones, las cuales probablemente mueva a otro
    archivo para que sea mas lindo de mirar mi main.c
    y agrego los struct de los pokemones, gimnasios, entrenadores y
    personaje pricipal

--- main.c
+++ main.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 
+#include "lista.h"
+
+#define MAX_NOMBRE 50
+#define MAX_POKE_COMBATE 6
+#define GIMNASIO "G"
+#define LIDER "L"
+#define POKEMON "P"
+#define ENTRENADOR "E"
+#define INGRESAR_ARCHIVO "E"
+#define AGREGAR_GIMNASIO "A"
+#define COMENZAR_PARTIDA "I"
+#define SIMULAR_PARTIDA "S"
+#define MOSTRAR_ENTRENADOR "E"
+#define MOSTRAR_GIMNASIO "G"
+#define CAMBIAR_POKEMON "C"
+#define PROXIMA_BATALLA "B"
+#define PROXIMO_COMBATE "N"
+
+typedef int (* funcion_batalla)(void*, void *);
+
+typedef struct pokemon{
+	int velocidad;
+	int defensa;
+	int ataque;
+} pokemon_t;
+
+typedef struct gimnasio {
+	char nombre[MAX_NOMBRE];
+	int dificultad;
+	int id_funcion;
+	lista_t* entrenadores; // es una pila
+	int cant_entrenadores;
+} gimnasio_t;
+
+typedef struct entrenador {
+	char nombre[MAX_NOMBRE];
+	lista_t* pokemones; // es una cola
+	int cant_pokemones;
+} entrenador_t;
+
+typedef struct personaje {
+	char nombre[MAX_NOMBRE];
+	lista_t* pokemones; // es una lista
+	int cant_pokemones;
+} personaje_t;
+
 int main() {
 
-    return 0;
+
+	return 0;
 }
