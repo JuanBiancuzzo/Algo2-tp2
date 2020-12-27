1da9e051cbfc6bebc9a806a8e3ca21c0614c2935
juan <jbiancuzzo@fi.uba.ar>
2020-12-27 12:32:31 -0300

    Creando pruebas para heap_crear
    Para ver si maneja bien los valores invalidos, y si inicializa
    correctamente los valores

--- pruebas/pruebas.c
+++ pruebas/pruebas.c
@@ -7,8 +7,54 @@
 #define ERROR -1
 #define EXITO 0
 
-void probar_heap_crear () {
+int comparador_prueba (void* elemento_uno, void* elemento_dos) {
+    return *(int*) elemento_uno > *(int*) elemento_dos ? 1 : *(int*) elemento_uno < *(int*) elemento_dos ? -1 : 0;
+}
 
+void destructor_prueba (void* arbol) {
+    arbol = arbol;
+}
+
+void probar_heap_crear_valores_invalidos () {
+    heap_comparador comparador = comparador_prueba;
+    heap_liberar_elemento destructor = destructor_prueba;
+    heap_t* heap = NULL;
+
+    pa2m_afirmar((heap = heap_crear(NULL, destructor)) == NULL,
+                 "Detecta correctamente que el comparador es invalido");
+
+    pa2m_afirmar((heap = heap_crear(comparador, NULL)) != NULL,
+                 "Detecta correctamente que no es necesario el destructor\n");
+
+    heap_destruir(heap);
+}
+
+void probar_heap_crear_inicializacion () {
+    heap_comparador comparador = comparador_prueba;
+    heap_liberar_elemento destructor = destructor_prueba;
+    heap_t* heap = NULL;
+
+    pa2m_afirmar((heap = heap_crear(comparador, destructor)) != NULL,
+                 "Mensaje de creacion correcta al crear el arbol");
+
+    pa2m_afirmar(!heap->elementos,
+                 "Los elementos se inicializan correctamente");
+
+    pa2m_afirmar(heap->cant_elementos == 0,
+                 "La cantidad de elementos se inicializan correctamente");
+
+    pa2m_afirmar(heap->comparador == comparador,
+                 "El comparador se inicializan correctamente");
+
+    pa2m_afirmar(heap->destructor == destructor,
+                 "El destructor se inicializan correctamente");
+
+    heap_destruir(heap);
+}
+
+void probar_heap_crear () {
+    probar_heap_crear_valores_invalidos();
+    probar_heap_crear_inicializacion();
 }
 
 void probar_heap_insertar () {
