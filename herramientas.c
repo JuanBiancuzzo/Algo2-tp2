925fd3ad81b287fd5c115c6d15331d62c313d0d5
juan <jbiancuzzo@fi.uba.ar>
2020-12-28 12:32:04 -0300

    Modificando archivo_2_personaje_principal
    Habian errores de typeo como poner || en vez de &&, la modularizacion
    que implemente antes solo generaba un ahorro de un par de linea y
    complicaba mas de lo que ayudaba

--- herramientas.c
+++ herramientas.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "herramientas.h"
 
@@ -13,10 +14,6 @@
 #define LEER_ID "%c;"
 #define CANT_ID 1
 
-int leer_id (FILE* archivo, char* id) {
-    return fscanf(archivo, LEER_ID, id);
-}
-
 int leer_pokemon(FILE* archivo, pokemon_t* pokemon) {
     return fscanf(archivo, LEER_POKEMON, pokemon->nombre, &(pokemon->velocidad), &(pokemon->defensa), &(pokemon->ataque));
 }
@@ -26,19 +23,11 @@ int leer_gimnasio (FILE* archivo, gimnasio_t* gimnasio) {
 }
 
 int leer_entrenador (FILE* archivo, char nombre[]) {
-
-    char id;
-    int leido = leer_id(archivo, &id);
-    if (leido != CANT_ID || id != ENTRENADOR) return ERROR;
-
-    leido = fscanf(archivo, LEER_ENTRENADOR, nombre);
-    if (leido != CANT_ENTRENADOR) return ERROR;
-
-    return EXITO;
+    return fscanf(archivo, LEER_ENTRENADOR, nombre);
 }
 
-pokemon_t* pokemon_crear() {
-    return calloc(1, sizeof(pokemon_t));
+int leer_id (FILE* archivo, char* id) {
+    return fscanf(archivo, LEER_ID, id);
 }
 
 int guardar_pokemones(FILE* archivo, lista_t* pokemones) {
@@ -47,9 +36,9 @@ int guardar_pokemones(FILE* archivo, lista_t* pokemones) {
     char id;
 
     int leido = leer_id(archivo, &id);
-    while (leido == CANT_ID || id == POKEMON) {
+    while (leido == CANT_ID && id == POKEMON) {
 
-        p_pokemon = pokemon_crear();
+        p_pokemon = calloc(1, sizeof(pokemon_t));
         if (!p_pokemon) return ERROR;
 
         leido = leer_pokemon(archivo, p_pokemon);
@@ -77,8 +66,13 @@ int archivo_2_personaje_principal (char ruta_archivo[], personaje_t* principal) 
     FILE* archivo = fopen(ruta_archivo, "r");
     if (!archivo) return ERROR;
 
-    int resultado = leer_entrenador(archivo, principal->nombre);
-    if (resultado == ERROR) return ERROR;
+    char id;
+    int leido = leer_id(archivo, &id), resultado;
+
+    if (leido != CANT_ID || id != ENTRENADOR) return ERROR;
+
+    leido = leer_entrenador(archivo, principal->nombre);
+    if (leido != CANT_ENTRENADOR) return ERROR;
 
     principal->pokemones = lista_crear();
     resultado = guardar_pokemones(archivo, principal->pokemones);
@@ -89,6 +83,7 @@ int archivo_2_personaje_principal (char ruta_archivo[], personaje_t* principal) 
     }
 
     principal->cant_pokemones = resultado;
+    fclose(archivo);
     return EXITO;
 }
 
