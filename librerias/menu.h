247792486ee930da59b03aec1561a78c7fdc53e3
juan <jbiancuzzo@fi.uba.ar>
2020-12-31 16:22:52 -0300

    Agregando los comentarios a las funciones

--- librerias/menu.h
+++ librerias/menu.h
@@ -22,14 +22,50 @@
 
 typedef void (*funcion_menu) (char[]);
 
+/*
+ * Teniendo las opciones de ingresar el archivo del personaje
+ * principal, ingresar el archivo de un gimnasio, comenzar la
+ * partida o simular la partida
+ *
+ * Devuelve los caracteres de las opciones que se muestran en
+ * el menu
+ */
 void menu_inicio(char instrucciones[]);
 
+/*
+ * Teniendo las opciones de mostrar el entrenador, mostrar el
+ * gimnasio, cambiar los pokemones y comenzar la batalla
+ *
+ * Devuelve los caracteres de las opciones que se muestran en
+ * el menu
+ */
 void menu_gimnasio(char instrucciones[]);
 
+/*
+ * La unica "opcion" es ir a la siguiente batalla
+ *
+ * Devuelve los caracteres de las opciones que se muestran en
+ * el menu
+ */
 void menu_batalla(char instrucciones[]);
 
+/*
+ * Teniendo las opciones de tomar prestado el pokemon del
+ * lider, cambiar el orden de los pokemones e ir al proximo
+ * gimnasio
+ *
+ * Devuelve los caracteres de las opciones que se muestran en
+ * el menu
+ */
 void menu_victoria(char instrucciones[]);
 
+/*
+ * Teniendo las opciones de cambiar el orden de los pokemones
+ * reintentar el gimnasio y finalizar la partida
+ *
+ * Devuelve los caracteres de las opciones que se muestran en
+ * el menu
+ */
 void menu_derrota(char instrucciones[]);
 
 #endif /* __HERRAMIENTAS_H__ */
