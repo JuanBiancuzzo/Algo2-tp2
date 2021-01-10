12e41ee7c0b7edee2f119946922dcb46f90c77c4
juan <jbiancuzzo@fi.uba.ar>
2021-01-10 17:46:36 -0300

    Agregando el comienzo del trabajo

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#ifdef __linux__
+#include <unistd.h>
+#define CLEAR system("clear")
+#define SLEEP sleep(1)
+#elif _WIN32
+#include <windows.h>
+#define CLEAR system("cls")
+#define SLEEP Sleep(100)
+#endif
 
 #include "librerias/herramientas.h"
 #include "librerias/batallas.h"
+#include "librerias/menu.h"
+
+#define CARPETA_GIMNASIO "gimnasios/"
+#define CARPETA_PRINCIPAL "principal/"
 
 int main() {
+    CLEAR;
 
-
-	return 0;
+    return 0;
 }
