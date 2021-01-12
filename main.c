24bdf69ac3dbcca59f7da2088f1df9b23da75b76
juan <jbiancuzzo@fi.uba.ar>
2021-01-12 11:59:09 -0300

    Inicializar el personaje principal
    Se encarga de preguntar al usuario el nombre del archivo, y si puede
    leerlo y no hubo ningun error, lo guarda en un personaje_t*

--- main.c
+++ main.c
@@ -52,6 +52,67 @@ char iniciar_juego () {
     }
     return respuesta;
 }
+
+void preguntar_archivo(char* carpeta_destino, int (*funcion_validacion) (char*, void*), void* auxilear) {
+
+    char ruta_archivo[MAX_NOMBRE], nombre_archivo[MAX_NOMBRE];
+    bool error = false;
+    FILE* archivo;
+
+    do {
+        CLEAR;
+        pantalla_titulo();
+
+        if (!error) {
+            printf("Por favor ingrese el nombre del archivo que tiene que estar en %s: ", carpeta_destino);
+        } else {
+            printf("El archivo que indicaste no es valido. Revisar si ingreso correctamente el nombre \n");
+            printf("del archivo o no coloco el archivo en la carpeta %s: ", carpeta_destino);
+        }
+
+        scanf(" %80[^\n]", nombre_archivo);
+
+        strcpy(ruta_archivo, carpeta_destino);
+        strcat(ruta_archivo, nombre_archivo);
+
+        archivo = fopen(ruta_archivo, "r");
+
+        error = false;
+        if (!archivo) error = true;
+
+        if (!error && funcion_validacion(ruta_archivo, auxilear) == ERROR) error = true;
+
+    } while (error);
+
+    fclose(archivo);
+}
+
+void inicializar_personaje(personaje_t* principal) {
+
+    char instrucciones[MAX_INSTRUC];
+    char respuesta;
+
+    do {
+        int (*funcion_validacion) (char*, void*) = archivo_2_personaje_principal;
+        preguntar_archivo(CARPETA_PRINCIPAL, funcion_validacion, principal);
+
+        CLEAR;
+        mostrar_principal(*principal);
+        menu_confirmacion(instrucciones, "Este es el personaje que queres usar?");
+        scanf(" %c", &respuesta);
+
+        while (!responder_opciones(instrucciones, respuesta)) {
+            CLEAR;
+            mostrar_principal(*principal);
+            menu_confirmacion(instrucciones, "Este es el personaje que queres usar?");
+            printf("Tenes que elegir una de las opciones\n");
+            scanf(" %c", &respuesta);
+        }
+
+    } while (responder_caracter(NEGAR, respuesta));
+
+}
+
 int main() {
     CLEAR;
 
